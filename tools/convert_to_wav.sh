d8004f545df3ca7e047e653a4c4a594aea09d5c1
hfaried <h.faried@pegb.tech>
2018-11-01 09:40:24 +0400

    merging to verse_downloader

--- tools/convert_to_wav.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-for filename in dataset/audio/*/*.mp3; do
-	echo "ffmpeg -i $filename -acodec pcm_u8 -ar 22050 $filename.wav"
-	ffmpeg -i $filename -acodec pcm_u8 -ar 22050 $filename.wav &>/dev/null
-done
